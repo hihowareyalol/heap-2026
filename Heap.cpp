9ae7417bfee349dca2ac23165f73e85aa3dfddc4
Colleen <crusting@horizon.csueastbay.edu>
2026-03-24 15:11:57 -0700

    heap, heap, reheap up

--- Heap.cpp
+++ Heap.cpp
@@ -1,7 +1,3 @@
-//
-// Created by we7289 on 3/13/2024.
-//
-
 #include "Heap.h"
 
 void swap(int *array, int i, int j) {
@@ -12,17 +8,43 @@ void swap(int *array, int i, int j) {
 
 template<class T>
 Heap<T>::Heap() {
-
+    size = 0;
+    max_size = DEFAULT_SIZE;
+    elements = new T[max_size];
 }
 
 template<class T>
 Heap<T>::Heap(int max_size) {
-
+    size = 0;
+    this->max_size = max_size;
+    elements = new T[max_size];
 }
 
 template<class T>
 void Heap<T>::ReheapUp(T value) {
+    if (size >= max_size)
+    {
+        throw FullHeap();
+    }
 
+    elements[size] = value;
+
+    //swap until parent is bigger
+    int currInd = size;
+
+    while (currInd != 0) {
+        int parInd = (currInd - 1) / 2;
+        //current is bigger than parent
+        if (elements[currInd] > elements[parInd])
+        {
+            swap(elements, currInd, parInd);
+        } else
+        {
+            //current is smaller than parent
+            break;
+        }
+    }
+    ++size;
 }
 
 template<class T>

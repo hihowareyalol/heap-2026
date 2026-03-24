f21640d6864f2ea1229585b8a45c32771fddea80
Colleen <crusting@horizon.csueastbay.edu>
2026-03-24 15:28:54 -0700

    reheap down

--- Heap.cpp
+++ Heap.cpp
@@ -49,7 +49,47 @@ void Heap<T>::ReheapUp(T value) {
 
 template<class T>
 T Heap<T>::ReheapDown() {
-    T priority_value;
+    T priority_value = elements[0];
+
+    elements[0] = elements[size - 1];
+    --size;
+
+    int currInd = 0;
+    int btmInd = size - 1;
+
+    int leftInd = currInd*2 + 1;
+    int rightInd = currInd*2 + 2;
+    int maxChildInd;
+
+    while (currInd != btmInd)
+    {
+        if (leftInd == btmInd)
+        {
+            /*if the bottom is the left child, then the
+            right branch doesn't exist. Then the max child is */
+            maxChildInd = leftInd;
+        }
+
+        if (elements[leftInd] > elements[rightInd])
+        {
+            maxChildInd = leftInd;
+        } else
+        {
+            maxChildInd = rightInd;
+        }
+
+        //compare curr w/ max child
+        if (elements[currInd] < elements[maxChildInd])
+        {
+            swap(elements, currInd, maxChildInd);
+            currInd = maxChildInd;
+        } else
+        {
+            //if we are bigger than the max
+            break;
+        }
+
+    }
 
     return priority_value;
 }
